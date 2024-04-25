3fcee1f537e58cd2fce576d39ec5bc881ac49b99
Eduardo Nathan <eduardo.nathan2018@outlook.com>
2024-04-24 23:12:00 -0300

    commit com meu avanço atual do modulo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import imageUser from "./assets/user-placeholder.jpg"
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    
+    <View 
+    style={styles.container}
+      //onTouchStart={(event)=>{Alert.alert("TOQUE", "Toque inicializado")}}
+      //onTouchEnd={(event)=>{Alert.alert("TOQUE", "Toque finalizado")}}
+      >
+
+      <View style={styles.userPhoto}>
+        <Image
+        style={styles.userPhoto}
+          source={imageUser}
+        />
+      </View>
+
+      <TextInput style={styles.inputStyle}
+        keyboardType='email-address'
+        placeholder='digite seu e-mail'/>
+
+      <TextInput style={styles.inputStyle}
+        keyboardType='default'
+        placeholder='digite sua senha'/>
+      
+      <View style={styles.button}>
+        <Text style={styles.text}>LOGIN</Text>
+      </View>
+      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection:'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color:'white',
+    fontSize:22,
+    fontWeight:'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#3344FF',
+    borderRadius:5,
+    padding:8,
+    marginTop:40,
+    marginBottom:400,
+    width:160,
+    height:40,
+    alignItems:'center'
+  },
+  userPhoto: {
+    borderRadius:200,
+    borderWidth:1,
+    borderColor:'grey',
+    width:200,
+    height:200
+  },
+  inputStyle: {
+    borderWidth:1,
+    marginTop:10,
+    borderColor:'grey',
+    width:200,
+    height:30,
+    textAlign:'center',
+  }
 });
